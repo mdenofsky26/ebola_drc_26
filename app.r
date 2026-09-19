7208742f7c7d76847dcedbe30d88b4a015bd2891
mdenofsky.github.io <fhd4@cdc.gov>
2026-09-19 15:33:33 -0400

    Initialize app with required libraries and data path
    Added necessary libraries and data path for ebola dataset.

--- app.r
+++ app.r
@@ -1,1 +1,8 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(readr)
 
+# Load data 
+data_path <- "https://github.com/mdenofsky26/ebola_drc_26/blob/main/data/drc_ebola_cases_consolidated.csv"
+data_path
